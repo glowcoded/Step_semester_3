3885da2bb08a4ccda4935cf2b3988cba7f2ad49e
glowcoded <pavithrak7149@gmail.com>
2026-08-29 12:08:40 +0530

    Add control flow assignment problems

---
+++ Step_semester_3/src/main/java/controlflow/assignment_problems/PositiveNegativeZero.java
@@ -0,0 +1,17 @@
+package controlflow.assigment_problems;
+
+public class PositiveNegativeZero {
+
+    public static void main(String[] args) {
+
+        int number = -5;
+
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
