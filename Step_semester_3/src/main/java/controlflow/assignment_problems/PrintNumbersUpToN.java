b66025406a2455a56e4d2da9581525794dc7b6f1
glowcoded <pavithrak7149@gmail.com>
2026-08-29 12:24:48 +0530

    Add control flow assignment problems

---
+++ Step_semester_3/src/main/java/controlflow/assignment_problems/PrintNumbersUpToN.java
@@ -0,0 +1,13 @@
+package controlflow.assigment_problems;
+
+public class PrintNumbersUpToN {
+
+    public static void main(String[] args) {
+
+        int n = 10;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
