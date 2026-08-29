682d3dce1820d79aa92ba26d6b7fcb664f0419d4
glowcoded <pavithrak7149@gmail.com>
2026-08-29 12:26:49 +0530

    Add control flow assignment problems

---
+++ Step_semester_3/src/main/java/controlflow/assignment_problems/SumOfNaturalNumbers.java
@@ -0,0 +1,18 @@
+package controlflow.assigment_problems;
+
+public class SumOfNaturalNumbers {
+
+    public static void main(String[] args) {
+
+        int n = 10;
+        int sum = 0;
+        int i = 1;
+
+        while (i <= n) {
+            sum = sum + i;
+            i++;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
